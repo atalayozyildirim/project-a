eae4875c033c84f92e2e1cd01308720f3bd02522
atalayozyildirim <atalayozyildirim@gmail.com>
2025-01-09 14:16:52 +0300

    Add global styles import to ProductsPage component

--- client/src/pages/products.tsx
+++ client/src/pages/products.tsx
@@ -1,5 +1,6 @@
 import { Layout } from "@/app/Components/Layout/Layout";
 import ProductsPage from "@/app/Components/MainComp/ProductsPage";
+import "./../style/globals.css";
 
 export default function index() {
   return (
